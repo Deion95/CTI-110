115d3af58e697ae1ed887cb66480b055e50132c1
Deion95 <150170182+Deion95@users.noreply.github.com>
2023-11-07 11:25:57 -0500

    First Upload

---
+++ P1LAB1_HendrixDeion.py
@@ -0,0 +1,9 @@
+#Deion Hendrix
+#11/7/2023
+#Basic Input/Output
+
+#Get Input from user
+name = input("Please Enter Your Name: " )
+
+#Display Ouput to user
+print("Hello", name, "Welcome to CTI-110")
