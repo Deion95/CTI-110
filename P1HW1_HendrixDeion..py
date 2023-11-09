a2010fb8cda22a3e4866c64a10acca4956609769
Deion95 <150170182+Deion95@users.noreply.github.com>
2023-11-09 10:57:42 -0500

    Add files via upload

---
+++ P1HW1_HendrixDeion..py
@@ -0,0 +1,21 @@
+#Deion Hendrix
+#11/7/2023
+#P1HW1
+
+
+#Get Input From User
+Number1=int(input("Enter whole number"))
+
+#Display Output
+print("You entered:", Number1)
+print(Number1,"Squared is", Number1**2)
+print(Number1,"Cubed is", Number1**3,"!!!") 
+
+#Get input from user
+Number2=int(input("Enter another number"))
+
+#Display Output
+print(Number1,"+",Number2, "is", Number1 + Number2)
+print(Number1,"*",Number2, "is", Number1 * Number2)
+
+
