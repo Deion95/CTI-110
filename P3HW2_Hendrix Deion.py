404ff403ba8086d1d6164fc9667cb8b82184c085
Deion95 <150170182+Deion95@users.noreply.github.com>
2023-11-28 11:46:57 -0500

    Add files via upload

---
+++ P3HW2_Hendrix Deion.py
@@ -0,0 +1,26 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:\Users\hendrixd5543\Downloads\P3HW2_Hendrix Deion.py =======
+Enter the employee name:Bob
+Enter the employee's hour: 40
+Enter employee's pay rate: 19.25
+------------------------------------------
+Employee Name: Bob
+
+
+Hours Worked  Pay Rate  Overtime  Overtime  Pay RegHour Pay   Gross Pay
+---------------------------------------------------------------------------------------------
+40  19.25   0     0.0    770.0    770.0
+>>> 
+======== RESTART: C:\Users\hendrixd5543\Downloads\P3HW2_Hendrix Deion.py =======
+Enter the employee name:Bob
+Enter the employee's hour: 40
+Enter employee's pay rate: 19.25
+------------------------------------------
+Employee Name: Bob
+
+
+Hours Worked  Pay Rate  Overtime  Overtime  Pay   RegHour Pay   Gross Pay
+---------------------------------------------------------------------------------------------
+40  19.25   0     0.0    770.0    770.0
